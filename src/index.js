980e00c77a03e9521d21ca77c8ee1ca0b32df64d
F0sternest <kevin.rod2026@gmail.com>
2022-11-25 09:02:01 -0500

    Usando use effect y poniendo un contador

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const Counter = () => {
-  const [mensaje, setMensaje] = useState('')
+  const [mensaje, setMensaje] = useState("");
+  const [contador, setContador] = useState(0);
+
+  useEffect(() => {
+    console.log("cambio");
+  }, [contador]);
+
   return (
     <div>
-      <input onChange={event => setMensaje(event.target.value)}/>
+      <input onChange={(event) => setMensaje(event.target.value)} />
       <button onClick={() => console.log(`El mensaje es: ${mensaje}`)}>
         Save
       </button>
+      <hr />
+
+      <div>
+        <h1>{`Contador: ${contador}`}</h1>
+        <button onClick={() => setContador(contador + 1)}>Agregar</button>
+      </div>
     </div>
   );
-}
+};
 
 root.render(
   <>
